379ef579c90bbf61faab67d6e1aae2eaefee7939
Matheus Silva <124784864+omaatheus@users.noreply.github.com>
2024-07-15 15:00:22 -0300

    🔨 refactor

--- womansDay.py
+++ womansDay.py
@@ -4,18 +4,16 @@ sexo = str(input('Insira o sexo (Homem/Mulher): ').lower())
 if(sexo == 'homem'):
     casado = str(input('Você é casado? (Sim/Não): ').lower())
 
-descontoMulher = (valor - ((valor * 20) / 100))
-descontoHomem = (valor - ((valor * 5) / 100))
+valorDescasado = f'{valor:.2f}'
+descontoMulher = f'{valor - (valor * 20) / 100:.2f}'
+descontoHomem = f'{valor - (valor * 5) / 100:.2f}'
 
-descontoHomemRefactor = f'{descontoHomem:.2f}'
-descontoMulherRefactor = f'{descontoMulher:.2f}'
-
-if((sexo == 'homem') and (casado == 'não')):
-    print(f'O Valor total da sua compra será de {valor}')
+if((sexo == 'homem') and (casado == 'não' or 'nao')):
+    print(f'O Valor total da sua compra será de R$100{valorDescasado}')
 
 if((sexo == 'homem') and (casado == 'sim')):
-    print(f'Você recebeu um desconto de 5% e o valor da sua compra ficou R${descontoHomemRefactor}')
+    print(f'Você recebeu um desconto de 5% e o valor da sua compra ficou R${descontoHomem}')
     
 if(sexo == 'mulher'):
-    print(f'Você recebeu um desconto de 20% e o valor da sua compra ficou R${descontoMulherRefactor} | Parabéns pelo dia das mulheres! ')
+    print(f'Você recebeu um desconto de 20% e o valor da sua compra ficou R${descontoMulher} | Parabéns pelo dia das mulheres! ')
     
